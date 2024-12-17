5969b06bdbfca9398a1fa93bc2b463002b4afa69
JSJm25 <jeffreymacmullin@gmail.com>
2024-12-17 10:55:53 -0800

    Add request logging middleware and header parser

--- index.js
+++ index.js
@@ -5,18 +5,29 @@ var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(express.static('public'));
-
+let d = new Date().toUTCString();
+app.use((req, res, done) =>{
+  console.log(`${req.method}    ${req.path} - ${req.ip} at ${d}`);
+  done();
+})
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  let ip = req.ip;
+  let language = req.headers["accept-language"];
+  let softWare = req.headers["user-agent"];
+  res.json({"ipaddress": ip, 
+            "language": language,
+            "software": softWare
+          });
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+  console.log(`Go to http://localhost:${listener.address().port}/`)
 });
